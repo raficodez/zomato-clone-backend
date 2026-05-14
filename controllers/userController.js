c123722028b0e9031fdf164f232deafc89adbba3
raficodez <shaikrafi9325@gmail.com>
2026-05-14 22:53:06 +0530

    Created user model and register database API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,24 @@
-const registerUser = (req, res) => {
-    let { name, email } = req.body;
+const userModel = require("../models/User")
 
-    res.status(201).json({
-        success: "true",
-        message: "User registered successfully",
-        user: {
+const registerUser = async (req, res) => {
+    try{
+
+        let { name, email, password } = req.body;
+
+        const user = await userModel.create({
             name,
-            email
-        }
-    });
+            email,
+            password
+        });
+    
+        res.status(201).json({
+            success: "true",
+            user
+        });
+    }
+    catch(error){
+
+    }
 };
 
 module.exports = {
