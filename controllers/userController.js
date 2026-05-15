8f7866d72e2fd6a97fcd6a5259c337f7fddb982a
raficodez <shaikrafi9325@gmail.com>
2026-05-15 12:36:05 +0530

    Implemented JWT authentication system

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,84 @@
 const userModel = require("../models/User")
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const registerUser = async (req, res) => {
-    try{
-
+    try {
         let { name, email, password } = req.body;
+        bcrypt.genSalt(10, (err, salt) => {
 
-        const user = await userModel.create({
-            name,
-            email,
-            password
-        });
-    
-        res.status(201).json({
-            success: "true",
-            user
-        });
+            bcrypt.hash(password, salt, async (err, hash) => {
+
+                const user = await userModel.create({
+                    name,
+                    email,
+                    password: hash,
+                });
+
+                const token = jwt.sign(
+                    { id: user._id },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '7d' }
+                );
+                res.status(201).json({
+                    success: true,
+                    token
+                })
+            })
+        })
     }
-    catch(error){
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
+const loginUser = async (req, res) => {
+    try {
 
+        let { email, password } = req.body;
+        let user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not found"
+            })
+        }
+
+        bcrypt.compare(password, user.password, (err, result) => {
+
+            if (!result) {
+
+                return res.status(400).json({
+                    success: false,
+                    message: "Invalid credentials"
+                });
+
+            };
+
+            const token = jwt.sign(
+                { id: user._id },
+                process.env.JWT_SECRET,
+                { expiresIn: '7d' }
+            );
+
+            res.status(200).json({
+                success: true,
+                token
+            })
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
 };
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
