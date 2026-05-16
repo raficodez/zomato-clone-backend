48c591770a4dff5ee3838e5730ab59c2f2b7fa80
raficodez <shaikrafi9325@gmail.com>
2026-05-16 11:50:16 +0530

    Added JWT middleware and protected profile route

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+
+const isLoggedIn = (req, res, next) => {
+
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "No token provided"
+            });
+        };
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch (error) {
+        res.status(401).json({
+            success: false,
+            message: "Invalid token"
+        })
+    };
+
+};
+
+module.exports = isLoggedIn;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const { registerUser, loginUser } = require('../controllers/userController');
+const isLoggedIn = require('../middlewares/authMiddleware')
 
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
+router.get('/profile', isLoggedIn, (req, res) => {
+
+    res.status(200).json({
+        success: true,
+        message: "Protected Profile Route",
+        user: req.user
+    })
+})
 
 
 module.exports = router;
